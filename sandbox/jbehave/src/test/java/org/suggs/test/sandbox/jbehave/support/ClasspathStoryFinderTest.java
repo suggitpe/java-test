62db492a28c8bc8d677fdefe7d764eee7c5fac16
Peter Suggitt <me@suggs.org.uk>
2013-08-13 21:32:23 +0100

    Fixed a bunch of issues in the gradle build

--- sandbox/jbehave/src/test/java/org/suggs/test/sandbox/jbehave/support/ClasspathStoryFinderTest.java
+++ sandbox/jbehave/src/test/java/org/suggs/test/sandbox/jbehave/support/ClasspathStoryFinderTest.java
@@ -1,6 +1,10 @@
 package org.suggs.test.sandbox.jbehave.support;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 import static org.hamcrest.Matchers.greaterThan;
 import static org.junit.Assert.assertThat;
@@ -8,8 +12,12 @@ import static org.suggs.test.sandbox.jbehave.support.ClasspathStoryFinder.findSt
 
 public class ClasspathStoryFinderTest {
 
+    private static final Logger LOG = LoggerFactory.getLogger(ClasspathStoryFinderTest.class);
+
     @Test
     public void shouldFindTestJavaFileFromClasspath() {
-        assertThat(findStories("**/*.xml").size(), greaterThan(1));
+        List<String> stories = findStories("**/*.xml");
+        LOG.debug("Found the following {} stories {}", stories.size(), stories);
+        assertThat(stories.size(), greaterThan(0));
     }
 }
