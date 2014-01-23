55705183949a5949bd710621ea5a1c160213b9f2
Peter Suggitt <me@suggs.org.uk>
2014-01-23 07:47:17 +0000

    First iteration of the Netted Agency Flow

---
+++ sandbox/concordion/src/test/java/org/suggs/test/sandbox/concordion/settlement/securitiessettlement/AgencyBusiness.java
@@ -0,0 +1,39 @@
+package org.suggs.test.sandbox.concordion.settlement.securitiessettlement;
+
+import org.agileinsider.concordion.junit.ConcordionPlus;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RunWith(ConcordionPlus.class)
+public class AgencyBusiness {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AgencyBusiness.class);
+
+    public void createBookingReportsWith(String flowType, Integer quantity) {
+        LOG.info("creating a {} for {}", flowType, quantity);
+    }
+
+    public String checkThatFundingHasBeenUpdatedWithPending(String text) {
+        return text;
+    }
+
+    public String checkThatFundingHasBeenUpdatedWithSettled(String text) {
+        return text;
+    }
+
+    public String checkOpenSettlementRiskPosted(String text){
+        return text;
+    }
+
+    public String checkCloseSettlementRiskPosted(String text){
+        return text;
+    }
+
+    public String checkBicBalances(String text){
+        return text;
+    }
+
+}
+
+
---
+++ sandbox/concordion/src/test/java/org/suggs/test/sandbox/concordion/settlement/securitiessettlement/Securitiessettlement.java
@@ -0,0 +1,8 @@
+package org.suggs.test.sandbox.concordion.settlement.securitiessettlement;
+
+import org.agileinsider.concordion.junit.ConcordionPlus;
+import org.junit.runner.RunWith;
+
+@RunWith(ConcordionPlus.class)
+public class Securitiessettlement {
+}
